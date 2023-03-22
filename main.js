433ea6c9e6052e51632584eef4c2110b39da0978
Megapari <alexander.front@megapari.com>
2023-03-22 18:50:31 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// smooth scrolling for same-page anchors - start
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+// smooth scrolling for same-page anchors - end
