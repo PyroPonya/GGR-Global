e714f38b4b5acadc2da964a4a584179d0f8859ef
Megapari <alexander.front@megapari.com>
2023-03-23 14:02:21 +0300

    day 1.5

--- main.js
+++ main.js
@@ -9,3 +9,73 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 // smooth scrolling for same-page anchors - end
+
+// swiper setup start
+ // swiper element
+ const swiperEl_web3 = document.querySelector('swiper-container#web3');
+  const swiperEl_paysys = document.querySelector('swiper-container#paysys');
+
+  // swiper parameters
+  const swiperParams = {
+    slidesPerView: 5,
+    freeMode: true,
+    spaceBetween: 20,
+    breakpoints: {
+      400: {
+        slidesPerView: 1,
+      },
+      740: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+      1320: {
+        slidesPerView: 4,
+      },
+      1440: {
+        slidesPerView: 5,
+      },
+    },
+    on: {
+      init() {
+        // ...
+      },
+    },
+  };
+  const swiperParams_short = {
+    slidesPerView: 5,
+    freeMode: true,
+    spaceBetween: 20,
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+      },
+      505: {
+        slidesPerView: 2,
+      },
+      670: {
+        slidesPerView: 3,
+      },
+      840: {
+        slidesPerView: 4,
+      },
+      1010: {
+        slidesPerView: 5,
+      },
+    },
+    on: {
+      init() {
+        // ...
+      },
+    },
+  };
+
+  // now we need to assign all parameters to Swiper element
+  Object.assign(swiperEl_web3, swiperParams_short);
+  Object.assign(swiperEl_paysys, swiperParams);
+
+  // and now initialize it
+  swiperEl_web3.initialize();
+  swiperEl_paysys.initialize();
+// swiper setup end
