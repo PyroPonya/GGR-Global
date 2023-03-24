81c51c8daf24f4382bd4287761ada591d11fee63
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2023-03-24 16:12:24 +0300

    todo: license, form

--- main.js
+++ main.js
@@ -1,81 +1,81 @@
 // smooth scrolling for same-page anchors - start
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
     });
+  });
 });
 // smooth scrolling for same-page anchors - end
 
 // swiper setup start
- // swiper element
- const swiperEl_web3 = document.querySelector('swiper-container#web3');
-  const swiperEl_paysys = document.querySelector('swiper-container#paysys');
+// swiper element
+const swiperEl_web3 = document.querySelector('swiper-container#web3');
+const swiperEl_paysys = document.querySelector('swiper-container#paysys');
 
-  // swiper parameters
-  const swiperParams = {
-    slidesPerView: 5,
-    freeMode: true,
-    spaceBetween: 20,
-    breakpoints: {
-      400: {
-        slidesPerView: 1,
-      },
-      740: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-      1320: {
-        slidesPerView: 4,
-      },
-      1440: {
-        slidesPerView: 5,
-      },
+// swiper parameters
+const swiperParams = {
+  slidesPerView: 1,
+  freeMode: true,
+  spaceBetween: 20,
+  breakpoints: {
+    400: {
+      slidesPerView: 1,
     },
-    on: {
-      init() {
-        // ...
-      },
+    740: {
+      slidesPerView: 2,
     },
-  };
-  const swiperParams_short = {
-    slidesPerView: 5,
-    freeMode: true,
-    spaceBetween: 20,
-    breakpoints: {
-      300: {
-        slidesPerView: 1,
-      },
-      505: {
-        slidesPerView: 2,
-      },
-      670: {
-        slidesPerView: 3,
-      },
-      840: {
-        slidesPerView: 4,
-      },
-      1010: {
-        slidesPerView: 5,
-      },
+    1024: {
+      slidesPerView: 3,
     },
-    on: {
-      init() {
-        // ...
-      },
+    1320: {
+      slidesPerView: 4,
     },
-  };
+    1440: {
+      slidesPerView: 5,
+    },
+  },
+  on: {
+    init() {
+      // ...
+    },
+  },
+};
+const swiperParams_short = {
+  slidesPerView: 1,
+  freeMode: true,
+  spaceBetween: 20,
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+    },
+    505: {
+      slidesPerView: 2,
+    },
+    670: {
+      slidesPerView: 3,
+    },
+    840: {
+      slidesPerView: 4,
+    },
+    1010: {
+      slidesPerView: 5,
+    },
+  },
+  on: {
+    init() {
+      // ...
+    },
+  },
+};
 
-  // now we need to assign all parameters to Swiper element
-  Object.assign(swiperEl_web3, swiperParams_short);
-  Object.assign(swiperEl_paysys, swiperParams);
+// now we need to assign all parameters to Swiper element
+Object.assign(swiperEl_web3, swiperParams_short);
+Object.assign(swiperEl_paysys, swiperParams);
 
-  // and now initialize it
-  swiperEl_web3.initialize();
-  swiperEl_paysys.initialize();
+// and now initialize it
+swiperEl_web3.initialize();
+swiperEl_paysys.initialize();
 // swiper setup end
